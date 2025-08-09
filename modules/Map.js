73537e2d1a2b5c915ea0b0ed6531240698dd9062
Marcelo <86026142+Joycemg@users.noreply.github.com>
2025-08-09 10:44:11 -0300

    Update Map.js

--- modules/Map.js
+++ modules/Map.js
@@ -50,7 +50,7 @@ if (!map) {
 
     const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         ...baseOptions,
-        attribution: '&copy; OpenStreetMap contributors'
+        attribution: '&copy; OpenStreetMap contributors', maxZoom: 18
     });
 
     const carto = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
@@ -190,5 +190,6 @@ if (!map) {
 }
 
 export { map };
+
 
 
